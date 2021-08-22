0599c46aabb3bd0b4ed6f784e16ec0a0b2cd00e8
danielgmateus <daniel.analista@outlook.com>
2021-08-22 12:36:02 -0300

    add new tests

--- sales/repositories.py
+++ sales/repositories.py
@@ -1,18 +1,12 @@
-from sales.services import SaleService
-
-
 class SaleRepository:
-    def __init__(self):
-        self.sale_service = SaleService()
-
     @staticmethod
-    def create(serializer: object, data):
+    def create(serializer: object, data: dict):
         instance = serializer.save(status=data.get('status'), cashback=data.get('cashback'),
                                    per_cent_cashback=data.get('per_cent_cashback'))
         instance.save()
 
     @staticmethod
-    def update(serializer: object, data):
+    def update(serializer: object, data: dict):
         instance = serializer.save(status=data.get('status'), cashback=data.get('cashback'),
                                    per_cent_cashback=data.get('per_cent_cashback'))
         instance.save()
